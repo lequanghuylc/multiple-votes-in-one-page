5140da827813667ab4c7b532a6b5dce1b5298327
quanghuyf <quanghuy@first-college.vn>
2016-07-28 21:33:35 +0000

    fixing security issues

--- form.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('../../../wp-blog-header.php');
-
-if (isset($_POST['postid']) && isset($_POST['countvalue'])){
-    update_post_meta($_POST['postid'],'_vote', $_POST['countvalue']);
-}
-
-?>
--- multiple-votes-in-one-page.php
+++ multiple-votes-in-one-page.php
@@ -10,52 +10,72 @@
  */
 ?>
 <?php
+
 // add shortcode
-function mul_votes_button($mul_votes_val) {
+function lqh_mul_votes_shortcode($val) {
 	
-	$countnumber = get_post_meta( $mul_votes_val['id'], '_vote', true );
-    $mul_votes_content = '<div>
-     <button id="mul_votes_'. $mul_votes_val['id'] .'" class="mul_votes_text">Vote</button>
-     <button id="mul_count_'. $mul_votes_val['id'] .'" class="mul_votes_val">' . $countnumber . '</button>
-     <iframe name="hidden_iframe" id="hidden_iframe" style="display:none;"onload="if(submitted)";}">
-	 </iframe>
-	 <form id="mul_votes_form_'. $mul_votes_val['id'] .'" action="'. plugins_url() .'/multiple-votes-in-one-page/form.php" method="post" target="hidden_iframe" onsubmit="submitted=true;">
-	 <input type="hidden" value="'. $mul_votes_val['id'] .'" name="postid">
-	 <input type="hidden" value="" name="countvalue" id="mul_votes_count_'. $mul_votes_val['id'] .'">
+	$countnumber = get_post_meta( $val['id'], '_vote', true );
+    $content = '<div>
+     <button id="lqh_mul_votes_'. $val['id'] .'" class="lqh_mul_votes_text">Vote</button>
+     <button id="lqh_mul_votes_count_'. $val['id'] .'" class="lqh_mul_votes_val">' . $countnumber . '</button>
+	 <form id="lqh_mul_votes_form_'. $val['id'] .'" method="post" action="">
+	 <input type="hidden" value="'. $val['id'] .'" name="lqh_mul_votes_postid">
+	 <input type="hidden" value="" name="lqh_mul_votes_countvalue" id="lqh_mul_votes_input_count_'. $val['id'] .'">
 	 </form>
      <script>
-        document.getElementById("mul_votes_'. $mul_votes_val['id'].'").onclick = function(){
-            var countNumber = document.getElementById("mul_count_'. $mul_votes_val['id'] .'");
-            countNumber.innerHTML = Number(countNumber.innerHTML) + 1;
-            this.setAttribute("disabled", "true");
-            this.innerHTML = "Voted";
-            document.getElementById("mul_votes_count_'. $mul_votes_val['id'] .'").value= countNumber.innerHTML;
-            document.getElementById("mul_votes_form_'. $mul_votes_val['id'] .'").submit();
-        }
+    	jQuery(function($){
+    		$("#lqh_mul_votes_'. $val['id'].'").click(function(){
+	            var countNumber = Number($("#lqh_mul_votes_count_'. $val['id'] .'").html()) + 1;
+	            $("#lqh_mul_votes_count_'. $val['id'] .'").html(countNumber);
+	            $("#lqh_mul_votes_input_count_'. $val['id'] .'").val(countNumber);
+	            $("#lqh_mul_votes_form_'. $val['id'] .'").submit();
+	
+	       });
+	       $("#lqh_mul_votes_form_'. $val['id'] .'").on("submit", function(){
+					$.ajax({
+						type:"POST",
+						url: "'.admin_url("admin-ajax.php", null).'",
+						data: $(this).serialize() + "&action=lqh_ajax_form_handle_from_shortcode",
+						success:function(data){
+							$("#lqh_mul_votes_'. $val['id'].'").html("Voted");
+							$("#lqh_mul_votes_'. $val['id'].'").attr("disabled", "true");
+						}
+					});
+				return false;	
+	       });
+	   });
      </script>
-     
      </div>';
 
-    return $mul_votes_content;
-
+    return $content;
 }
-add_shortcode( 'mul_votes', 'mul_votes_button' );
+add_shortcode( 'lqh_mul_votes', 'lqh_mul_votes_shortcode' );
 
+// AJAX Handling Form from Shortcode
 
-// add custom post type Vote and Vote taxonomy
-function create_posttype() {
+function lqh_ajax_form_handle_from_shortcode(){
+	$lqh_post_id= absint(intval(sanitize_text_field($_POST['lqh_mul_votes_postid'])));
+	$lqh_post_count_value = absint(intval(sanitize_text_field($_POST['lqh_mul_votes_countvalue'])));
+	update_post_meta($lqh_post_id,'_vote', $lqh_post_count_value);
+	die();
+}
+add_action('wp_ajax_lqh_ajax_form_handle_from_shortcode', 'lqh_ajax_form_handle_from_shortcode');
+add_action('wp_ajax_nopriv_lqh_ajax_form_handle_from_shortcode', 'lqh_ajax_form_handle_from_shortcode');
+
+// add custom post type Vote and its taxonomy
+function lqh_mul_votes_create_vote_post_type() {
 	
 	$labels = array(
-			'name'               => __( 'Votes', 'vote-type' ),
-			'singular_name'      => __( 'Vote', 'vote-type' ),
-			'add_new'            => __( 'Add Vote', 'vote-type' ),
-			'add_new_item'       => __( 'Add Vote', 'vote-type' ),
-			'edit_item'          => __( 'Edit Vote', 'vote-type' ),
-			'new_item'           => __( 'New Vote', 'vote-type' ),
-			'view_item'          => __( 'View Vote', 'vote-type' ),
-			'search_items'       => __( 'Search Vote', 'vote-type' ),
-			'not_found'          => __( 'No votes found', 'vote-type' ),
-			'not_found_in_trash' => __( 'No votes in the trash', 'vote-type' ),
+			'name'               => __( 'Votes' ),
+			'singular_name'      => __( 'Vote' ),
+			'add_new'            => __( 'Add Vote' ),
+			'add_new_item'       => __( 'Add Vote' ),
+			'edit_item'          => __( 'Edit Vote' ),
+			'new_item'           => __( 'New Vote' ),
+			'view_item'          => __( 'View Vote' ),
+			'search_items'       => __( 'Search Vote' ),
+			'not_found'          => __( 'No votes found' ),
+			'not_found_in_trash' => __( 'No votes in the trash' ),
 		);
 		$supports = array(
 			'title'
@@ -65,32 +85,32 @@ function create_posttype() {
 			'supports'        => $supports,
 			'public'          => true,
 			'capability_type' => 'post',
-			'rewrite'         => array( 'slug' => 'vote', ),
+			'rewrite'         => array( 'slug' => 'votes-iop-lqh', ),
 			'menu_position'   => 30,
 			'menu_icon'       => 'dashicons-thumbs-up',
-			'register_meta_box_cb' => 'add_votes_metaboxes'
+			'register_meta_box_cb' => 'lqh_mul_votes_add_metaboxes'
 		);
 	
 	
-	register_post_type( 'votes', $args	);
+	register_post_type( 'multiplevotesioplqh', $args	);
 	
 	$labels2 = array(
-			'name'                       => __( 'Vote Categories', 'vote-type' ),
-			'singular_name'              => __( 'Vote Category', 'vote-type' ),
-			'menu_name'                  => __( 'Vote Categories', 'vote-type' ),
-			'edit_item'                  => __( 'Edit Vote Category', 'vote-type' ),
-			'update_item'                => __( 'Update Vote Category', 'vote-type' ),
-			'add_new_item'               => __( 'Add New Vote Category', 'vote-type' ),
-			'new_item_name'              => __( 'New Voteeam Category Name', 'vote-type' ),
-			'parent_item'                => __( 'Parent Vote Category', 'vote-type' ),
-			'parent_item_colon'          => __( 'Parent Vote Category:', 'vote-type' ),
-			'all_items'                  => __( 'All Vote Categories', 'vote-type' ),
-			'search_items'               => __( 'Search Vote Categories', 'vote-type' ),
-			'popular_items'              => __( 'Popular Vote Categories', 'vote-type' ),
-			'separate_items_with_commas' => __( 'Separate Vote categories with commas', 'vote-type' ),
-			'add_or_remove_items'        => __( 'Add or remove Vote categories', 'vote-type' ),
-			'choose_from_most_used'      => __( 'Choose from the most used Vote categories', 'vote-type' ),
-			'not_found'                  => __( 'No Vote categories found.', 'vote-type' ),
+			'name'                       => __( 'Vote Categories' ),
+			'singular_name'              => __( 'Vote Category' ),
+			'menu_name'                  => __( 'Vote Categories' ),
+			'edit_item'                  => __( 'Edit Vote Category' ),
+			'update_item'                => __( 'Update Vote Category' ),
+			'add_new_item'               => __( 'Add New Vote Category' ),
+			'new_item_name'              => __( 'New Voteeam Category Name' ),
+			'parent_item'                => __( 'Parent Vote Category' ),
+			'parent_item_colon'          => __( 'Parent Vote Category:' ),
+			'all_items'                  => __( 'All Vote Categories' ),
+			'search_items'               => __( 'Search Vote Categories' ),
+			'popular_items'              => __( 'Popular Vote Categories' ),
+			'separate_items_with_commas' => __( 'Separate Vote categories with commas' ),
+			'add_or_remove_items'        => __( 'Add or remove Vote categories' ),
+			'choose_from_most_used'      => __( 'Choose from the most used Vote categories' ),
+			'not_found'                  => __( 'No Vote categories found.' ),
 		);
 		$args2 = array(
 			'labels'            => $labels2,
@@ -99,19 +119,19 @@ function create_posttype() {
 			'show_ui'           => true,
 			'show_tagcloud'     => true,
 			'hierarchical'      => true,
-			'rewrite'           => array( 'slug' => 'vote-category' ),
+			'rewrite'           => array( 'slug' => 'votes-iop-lqh-category' ),
 			'show_admin_column' => true,
 			'query_var'         => true,
 		);
 		$args2 = apply_filters( 'vote_post_type_category_args', $args2 );
 	
-	register_taxonomy( 'vote-categories', 'votes', $args2 );
+	register_taxonomy( 'votes-iop-lqh-categories', 'multiplevotesioplqh', $args2 );
 	
 	// Add the Vote Count Meta Boxes and show Shortcode
-	function add_votes_metaboxes() {
-		add_meta_box('vote-count', 'Vote Count', 'mul_vote_count', 'votes', 'normal', 'high');
+	function lqh_mul_votes_add_metaboxes() {
+		add_meta_box('lqh-vote-count', 'Vote Count', 'lqh_mul_votes_add_metabox_count', 'multiplevotesioplqh', 'normal', 'high');
 	}
-	function mul_vote_count() {
+	function lqh_mul_votes_add_metabox_count() {
 		global $post;
 	
 		// Noncename needed to verify where the data originated
@@ -126,17 +146,20 @@ function create_posttype() {
 		<p><em>Note: The default count value is 0 when creating vote and it will increase when viewers vote</em></p>
 		<p><em>You can change its nature value by editting the field</em></p>
 		<h4>Vote Shortcode</h4>
-		<input id="shortcode-generate" type="text" value=\'[mul_votes id="' . $post->ID  . '"]\'  readonly/>
+		<input id="shortcode-generate" type="text" value=\'[lqh_mul_votes id="' . $post->ID  . '"]\'  readonly/>
 		<span id ="copy-shortcode" class="btn" data-clipboard-target="#shortcode-generate">Copy Shortcode</span>
-		<script>new Clipboard(".btn");
-		document.getElementById("copy-shortcode").onclick= function(){this.innerHTML = "Shortcode Copied";};
+		<script>
+		window.onload=function(){
+			new Clipboard(".btn");
+			document.getElementById("copy-shortcode").onclick= function(){this.innerHTML = "Shortcode Copied";};
+		};
 		</script>
-		<p><em>If you want to use your own style, customize class "<strong>mul_votes_text</strong>" (for button Vote) and "<strong>mul_votes_val</strong>" (for the Vote count ) in your custom CSS</em></p>';
+		<p><em>If you want to use your own style, customize class "<strong>lqh_mul_votes_text</strong>" (for button Vote) and "<strong>lqh_mul_votes_val</strong>" (for the Vote count ) in your custom CSS</em></p>';
 
 	}
 	
 	// Save the Metabox Data
-	function wpt_save_votes_meta($post_id, $post) {
+	function lqh_mul_votes_save_meta_content($post_id, $post) {
 		
 		// verify this came from the our screen and with proper authorization,
 		// because save_post can be triggered at other times
@@ -151,11 +174,11 @@ function create_posttype() {
 		// OK, we're authenticated: we need to find and save the data
 		// We'll put it into an array to make it easier to loop though.
 		
-		$votes_meta['_vote'] = $_POST['_vote'];
+		$multiplevotesioplqh_meta['_vote'] = $_POST['_vote'];
 		
 		// Add values of $events_meta as custom fields
 		
-		foreach ($votes_meta as $key => $value) { // Cycle through the $votes_meta array!
+		foreach ($multiplevotesioplqh_meta as $key => $value) { // Cycle through the $multiplevotesioplqh_meta array!
 			if( $post->post_type == 'revision' ) return; // Don't store custom data twice
 			$value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
 			if(get_post_meta($post->ID, $key, FALSE)) { // If the custom field already has a value
@@ -168,26 +191,14 @@ function create_posttype() {
 	
 	}
 
-add_action('save_post', 'wpt_save_votes_meta', 1, 2); // save the custom fields
+add_action('save_post', 'lqh_mul_votes_save_meta_content', 1, 2); // save the custom fields
 }
-add_action( 'init', 'create_posttype' );
-
-// remove permark link, add some style
-add_action('admin_head', 'remove_vote_permarklink');
-function remove_vote_permarklink() {
-
-    global $post_type;
-
-    if ($post_type == 'votes') {
-    	echo '<link rel="stylesheet" type="text/css" href="'. plugins_url(). '/multiple-votes-in-one-page/assets/style.css' . '">';
-        echo '<script src="'. plugins_url() .'/multiple-votes-in-one-page/assets/clipboard.min.js"></script>';
-    }
-}
+add_action( 'init', 'lqh_mul_votes_create_vote_post_type' );
 
 // add column to show count content in admin page
-add_filter( 'manage_edit-votes_columns', 'edit_vote_columns' ) ;
+add_filter( 'manage_edit-multiplevotesioplqh_columns', 'lqh_mul_votes_add_columns_admin' ) ;
 
-function edit_vote_columns( $columns ) {
+function lqh_mul_votes_add_columns_admin( $columns ) {
 
 	$columns = array_slice($columns, 0, 2, true) +
     array('count' => __( 'Count Number' ), 'short' => __( 'Shortcode' )) +
@@ -195,9 +206,9 @@ function edit_vote_columns( $columns ) {
 	return $columns;
 }
 
-add_action( 'manage_votes_posts_custom_column', 'manage_votes_columns', 10, 2 );
+add_action( 'manage_multiplevotesioplqh_posts_custom_column', 'lqh_mul_votes_manage_columns_admin', 10, 2 );
 
-function manage_votes_columns( $column, $post_id ) {
+function lqh_mul_votes_manage_columns_admin( $column, $post_id ) {
 	global $post;
 
 	switch( $column ) {
@@ -209,7 +220,7 @@ function manage_votes_columns( $column, $post_id ) {
 			printf($countnumber );
 			break;
 		case 'short' :
-			printf('[mul_votes id="'. $post_id .'"]');
+			printf('[lqh_mul_votes id="'. $post_id .'"]');
 			break;
 
 		/* Just break out of the switch statement for everything else. */
@@ -218,16 +229,16 @@ function manage_votes_columns( $column, $post_id ) {
 	}
 }
 
-add_filter( 'manage_edit-votes_sortable_columns', 'votes_sortable_columns' );
+add_filter( 'manage_edit-multiplevotesioplqh_sortable_columns', 'lqh_mul_votes_make_columns_sortable' );
 
-function votes_sortable_columns( $columns ) {
+function lqh_mul_votes_make_columns_sortable( $columns ) {
 
 	$columns['count'] = '_vote';
 
 	return $columns;
 }
-add_action( 'pre_get_posts', 'manage_wp_posts_be_qe_pre_get_posts', 1 );
-function manage_wp_posts_be_qe_pre_get_posts( $query ) {
+add_action( 'pre_get_posts', 'lqh_mul_votes_handle_sorting', 1 );
+function lqh_mul_votes_handle_sorting( $query ) {
 
    if ( $query->is_main_query() && ( $orderby = $query->get( 'orderby' ) ) ) {
 
@@ -249,12 +260,12 @@ function manage_wp_posts_be_qe_pre_get_posts( $query ) {
 
 }
 
-//filter by categories
-add_action('restrict_manage_posts', 'filter_votes_by_category');
-function filter_votes_by_category() {
+//add filter by categories to admin
+add_action('restrict_manage_posts', 'lqh_mul_votes_add_filter_by_categories');
+function lqh_mul_votes_add_filter_by_categories() {
 	global $typenow;
-	$post_type = 'votes'; // change to your post type
-	$taxonomy  = 'vote-categories'; // change to your taxonomy
+	$post_type = 'multiplevotesioplqh'; // change to your post type
+	$taxonomy  = 'votes-iop-lqh-categories'; // change to your taxonomy
 	if ($typenow == $post_type) {
 		$selected      = isset($_GET[$taxonomy]) ? $_GET[$taxonomy] : '';
 		$info_taxonomy = get_taxonomy($taxonomy);
@@ -269,16 +280,11 @@ function filter_votes_by_category() {
 		));
 	};
 }
-/**
- * Filter posts by taxonomy in admin
- * @author  Mike Hemberger
- * @link http://thestizmedia.com/custom-post-type-filter-admin-custom-taxonomy/
- */
-add_filter('parse_query', 'tsm_convert_id_to_term_in_query');
-function tsm_convert_id_to_term_in_query($query) {
+add_filter('parse_query', 'lqh_mul_votes_handle_filtering');
+function lqh_mul_votes_handle_filtering($query) {
 	global $pagenow;
-	$post_type = 'votes'; // change to your post type
-	$taxonomy  = 'vote-categories'; // change to your taxonomy
+	$post_type = 'multiplevotesioplqh'; // change to your post type
+	$taxonomy  = 'votes-iop-lqh-categories'; // change to your taxonomy
 	$q_vars    = &$query->query_vars;
 	if ( $pagenow == 'edit.php' && isset($q_vars['post_type']) && $q_vars['post_type'] == $post_type && isset($q_vars[$taxonomy]) && is_numeric($q_vars[$taxonomy]) && $q_vars[$taxonomy] != 0 ) {
 		$term = get_term_by('id', $q_vars[$taxonomy], $taxonomy);
@@ -286,9 +292,23 @@ function tsm_convert_id_to_term_in_query($query) {
 	}
 }
 
-//add link tag in header
-add_action( 'wp_head', 'mul_votes_style' );
-function mul_votes_style() {
-  echo '<link rel="stylesheet" type="text/css" href="'. plugins_url(). '/multiple-votes-in-one-page/assets/style.css' . '">';
+//add style and script to front
+add_action( 'wp_head', 'lqh_mul_votes_style_and_script_front' );
+function lqh_mul_votes_style_and_script_front() {
+  wp_enqueue_script('jquery');
+  wp_enqueue_style('lqh_mul_votes_style_front_style', plugins_url('/assets/style.css', __FILE__) );
 }
+
+// adding style and script to admin
+add_action('admin_head', 'lqh_mul_votes_style_and_script_admin');
+function lqh_mul_votes_style_and_script_admin() {
+
+    global $post_type;
+
+    if ($post_type == 'multiplevotesioplqh') {
+    	wp_enqueue_style('lqh_mul_votes_style_front_style', plugins_url('/assets/style.css', __FILE__) );
+    	wp_enqueue_script('lqh_mul_votes_script_clipboard', plugins_url('/assets/clipboard.min.js', __FILE__) );
+    }
+}
+
 ?>
